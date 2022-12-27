56009d7b03c58f3f945a500f2a2466326110ff35
liar780@gmail.com <liar780@gmail.com>
2022-12-27 20:00:56 +0700

    create post

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -6,15 +6,17 @@ import {
 } from 'react-router-dom';
 import './style.css';
 import { Home } from './pages/Home';
+import { CreatePost } from './pages/CreatePost';
 
 function App() {
 	return (
 		<div>
 			<Router>
+				<Link to='/'>Home</Link>
 				<Link to='/createPost'>Create post</Link>
 				<Routes>
 					<Route path='/' exact element={<Home />} />
-					<Route path='/createPost' element={<Home />} />
+					<Route path='/createPost' element={<CreatePost />} />
 					<Route path='*' element={<div><h1>NOTTING</h1></div>} />
 				</Routes>
 			</Router>
---
+++ frontEnd/src/pages/CreatePost.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+export const CreatePost = () => {
+  const initialValues = {
+    title: '',
+    postText: '',
+    username: ''
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required('tolong masukan judul'),
+    postText: Yup.string().required('tolong masukan teks'),
+    username: Yup.string().min(3).max(15).required('tolong masukan nama anda')
+  });
+
+
+
+
+  const onSubmit = (data) => {
+
+    axios.post('http://localhost:5000/posts', data)
+      .then(res => {
+        console.log('success' + data);
+      });
+
+    console.log(data);
+  };
+
+  return (
+    <div className='createPostPage'>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className='formContainer'>
+          <label>Title : </label>
+          <ErrorMessage
+            name='title'
+            component={'span'} />
+          <Field
+
+            autoComplete='off'
+            id='inputCreatePost'
+            name='title'
+            placeholder='(Ex Title)' />
+
+          <label>Post : </label>
+          <ErrorMessage
+            name='postText'
+            component={'span'} />
+
+          <Field
+            autoComplete='off'
+            id='inputCreatePost'
+            name='postText'
+            placeholder='(Ex postText)' />
+
+          <label>username : </label>
+          <ErrorMessage
+            name='username'
+            component={'span'} />
+
+          <Field
+            autoComplete='off'
+            id='inputCreatePost'
+            name='username'
+            placeholder='(Ex username)' />
+          <button type='submit'>create post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};;
