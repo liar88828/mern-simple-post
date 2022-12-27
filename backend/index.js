20aadec0462a2086a6135d03e55362d5d0d524f7
liar780@gmail.com <liar780@gmail.com>
2022-12-27 14:42:28 +0700

    post and get

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
-const port = 6000;
+const port = 5000;
 const db = require('./models');
 
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
+
+//Router
+const postRouter = require('./routes/Posts');
+app.use('/posts', postRouter);
 
 db.sequelize.sync().then(() => {
 	app.listen(port, () => {
